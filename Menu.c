b25695adde788ef25864f40ee51c725584d68a35
soaresmiguel <miguelpachecosoares@enta.pt>
2019-12-04 12:20:54 -0100

    Menu.c

---
+++ Menu.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf(" 1-\t Clientes \n");
+      printf(" 2-\t Fornecedores \n");
+        printf(" 3-\t Fatura \n");
+          printf(" 4-\t Sair \n");
+         return 0;  
+}
